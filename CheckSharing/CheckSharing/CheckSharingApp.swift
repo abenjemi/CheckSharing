be5e47051aabd116ec2fb3d77f6df6e37dc197b0
Amine Ben Jemia <aminebenjemia8@gmail.com>
2022-02-26 20:31:58 -0500

    created the app

---
+++ CheckSharing/CheckSharing/CheckSharingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckSharingApp.swift
+//  CheckSharing
+//
+//  Created by Amine Ben Jemia on 2/26/22.
+//
+
+import SwiftUI
+
+@main
+struct CheckSharingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CheckSharing/CheckSharing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CheckSharing
+//
+//  Created by Amine Ben Jemia on 2/26/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
